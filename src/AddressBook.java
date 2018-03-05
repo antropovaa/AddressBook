4a2e64cbd7d8e583c6f39e0512ebf8ad91c8b368
antropovaa <anusy.99@yandex.ru>
2018-03-05 15:37:04 +0300

    fixed tests and added javadoc

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,6 +1,12 @@
 import java.util.*;
 
+/**
+ * Create an address book, where person's name and address are stored.
+ */
 public class AddressBook {
+    /**
+     * Create an address of person by street, building and apartment.
+     */
     public class Address {
         private String street;
         private int building;
@@ -43,53 +49,97 @@ public class AddressBook {
             result = 31 * result + apartment;
             return result;
         }
+
+        @Override
+        public String toString() {
+            return street + ", " + building + ", " + apartment;
+        }
     }
 
-    private String name;
     private Map<String, Address> addressBook;
 
     AddressBook() {
-        this.addressBook = new HashMap<String, Address>();
+        this.addressBook = new HashMap<>();
     }
 
-    // Добавить нового человека в адресную книгу
+    /**
+     * Add new record to address book by full information of his address.
+     * @param name Name of person who are added to address book.
+     * @param street Street on which person lives.
+     * @param building Building on street in which person lives.
+     * @param apartment Apartment of building.
+     */
     public void addNewRecord(String name, String street, int building, int apartment) {
-        Address address = new Address(street, building, apartment);
-        addressBook.put(name, address);
+        if (name == null || street == null)
+            throw new NullPointerException();
+        if (!addressBook.containsKey(name)) {
+            Address address = new Address(street, building, apartment);
+            addressBook.put(name, address);
+        }
     }
 
-    // Удалить человека из адресной книги
+    /**
+     * Delete person's information from address book by his name.
+     * @param name Name of person who are deleted from address book.
+     */
     public void deleteRecord(String name) {
+        if (name == null || !addressBook.containsKey(name))
+            throw new NullPointerException();
         addressBook.remove(name);
     }
 
-    // Изменить адрес человека
-    public void changeAddress(String name, String street, int building, int apartment){
+    /**
+     * Change person's address by replacing his old full information with new.
+     * @param name Name of person whose address will be changed.
+     * @param street New street of address.
+     * @param building New building of address.
+     * @param apartment New apartment of address.
+     */
+    public void changeAddress(String name, String street, int building, int apartment) {
+        if (name == null || street == null || building == 0 || apartment == 0 || !addressBook.containsKey(name))
+            throw new NullPointerException();
         Address newAddress = new Address(street, building, apartment);
         addressBook.put(name, newAddress);
     }
 
-    // Найти адрес человека по имени
-    public String searchByName(String name) {
-        Address address = addressBook.get(name);
-        return address.getStreet() + ", " + address.getBuilding() + ", " + address.getApartment();
+    /**
+     * Search person's address in address book by his name.
+     * @param name Name of person whose address need to be found.
+     * @return Found address.
+     */
+    public Address searchByName(String name) {
+        if (name == null || !addressBook.containsKey(name))
+            throw new NullPointerException();
+        return addressBook.get(name);
     }
 
-    // Вывести список людей, живущих на заданной улице
+    /**
+     * Output list of people's name who live on given street.
+     * @param street Street on which the search is carried out.
+     * @return List of founded names.
+     */
     public List<String> searchByStreet(String street) {
+        if (street == null)
+            throw new NullPointerException();
         List<String> searchResult = new ArrayList<>();
-        Collection<String> allNames = addressBook.keySet();
-            for (String currentName : allNames) {
-                String currentStreet = addressBook.get(currentName).getStreet();
-                if (currentStreet.equals(street)) {
-                    searchResult.add(currentName);
-                }
-            }
-            return searchResult;
+        Set<Map.Entry<String, Address>> allAddress = addressBook.entrySet();
+        for (Map.Entry<String, Address> currentAddress : allAddress) {
+            String currentStreet = currentAddress.getValue().getStreet();
+            if (currentStreet.equals(street))
+                searchResult.add(currentAddress.getKey());
+        }
+        return searchResult;
     }
 
-    // Вывести список людей, живущих в заданном доме
+    /**
+     * Output list of people's name who live on given building of giving street.
+     * @param street Street on which the search is carried out.
+     * @param building Building on which the search is carried out.
+     * @return List of names.
+     */
     public List<String> searchByBuilding(String street, int building) {
+        if (street == null || building == 0)
+            throw new NullPointerException();
         List<String> allNames = searchByStreet(street);
         List<String> searchResult = new ArrayList<>();
         for (String currentName : allNames) {
--- test/AddressBookTest.java
+++ test/AddressBookTest.java
@@ -5,69 +5,79 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.*;
 
 class AddressBookTest {
+    AddressBook book = new AddressBook();
     @Test
     void addNewRecord() {
-        AddressBook book = new AddressBook();
         book.addNewRecord("Аня", "Горького", 150, 26);
         book.addNewRecord("Петя", "Ленина", 104, 2);
         book.addNewRecord("Света", "Советская", 32, 13);
-        assertEquals("Горького, 150, 26", book.searchByName("Аня"));
-        assertEquals("Ленина, 104, 2", book.searchByName("Петя"));
-        assertEquals("Советская, 32, 13", book.searchByName("Света"));
+        assertEquals("Горького, 150, 26", book.searchByName("Аня").toString());
+        assertEquals("Ленина, 104, 2", book.searchByName("Петя").toString());
+        assertEquals("Советская, 32, 13", book.searchByName("Света").toString());
+        assertThrows(NullPointerException.class, () -> {
+                book.addNewRecord("Аня", "Горького", 150, 26);
+                book.addNewRecord(null, null, 32, 13);
+        });
     }
 
     @Test
     void deleteRecord() throws NullPointerException {
-        AddressBook book = new AddressBook();
         book.addNewRecord("Аня", "Горького", 150, 26);
         book.addNewRecord("Петя", "Ленина", 104, 2);
         book.addNewRecord("Света", "Советская", 32, 13);
         book.deleteRecord("Света");
-        try {
+        assertThrows(NullPointerException.class, () -> {
+            book.deleteRecord(null);
+            book.deleteRecord("Маша");
             book.searchByName("Света");
-            fail("Expected NullPointerException");
-        } catch (NullPointerException thrown) {
-            assertNotEquals("", thrown.getMessage());
-        }
+        });
     }
 
     @Test
     void changeAddress() {
-        AddressBook book = new AddressBook();
         book.addNewRecord("Аня", "Горького", 150, 26);
         book.changeAddress("Аня", "Пушкина", 4, 98);
-        assertEquals("Пушкина, 4, 98", book.searchByName("Аня"));
+        assertEquals("Пушкина, 4, 98", book.searchByName("Аня").toString());
+        assertThrows(NullPointerException.class, () -> {
+            book.changeAddress("Маша", "Ленина", 1, 23);
+            book.changeAddress(null, "Советская", 0, 0);
+        });
     }
 
     @Test
     void searchByName() {
-        AddressBook book = new AddressBook();
         book.addNewRecord("Аня", "Горького", 150, 26);
         book.addNewRecord("Петя", "Ленина", 104, 2);
         book.addNewRecord("Света", "Советская", 32, 13);
-        assertEquals("Горького, 150, 26", book.searchByName("Аня"));
+        assertEquals("Горького, 150, 26", book.searchByName("Аня").toString());
+        assertThrows(NullPointerException.class, () -> {
+            book.searchByName("Маша");
+            book.searchByName(null);
+        });
     }
 
     @Test
     void searchByStreet() {
-        AddressBook book = new AddressBook();
         List<String> expected = new ArrayList<>();
         Collections.addAll(expected,"Петя", "Аня");
         book.addNewRecord("Аня", "Горького", 150, 26);
         book.addNewRecord("Петя", "Горького", 150, 26);
         book.addNewRecord("Света", "Советская", 32, 13);
         assertEquals(expected, book.searchByStreet("Горького"));
+        assertThrows(NullPointerException.class, () -> book.searchByStreet(null));
     }
 
     @Test
     void searchByBuilding() {
-        AddressBook book = new AddressBook();
         List<String> expected = new ArrayList<>();
         Collections.addAll(expected,"Петя", "Аня");
         book.addNewRecord("Аня", "Горького", 150, 26);
         book.addNewRecord("Петя", "Горького", 150, 26);
         book.addNewRecord("Света", "Советская", 32, 13);
         assertEquals(expected, book.searchByBuilding("Горького", 150));
+        assertThrows(NullPointerException.class, () -> {
+            book.searchByBuilding(null, 12);
+            book.searchByBuilding("Советская", 0);
+        });
     }
-
 }
